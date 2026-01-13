4ba259179914b5d16377ae4919438ddd6ae104fb
tonye4 <tpt.tran7@gmail.com>
2026-01-13 11:21:11 -0500

    create variation of TextL to recursively add formatting to the string

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 type Reading struct {
 	Date  string
 	Title string
-	Body  []string // consider making it a slice of paragraphs.
+	Body  string // consider making it a slice of paragraphs.
 	URL   string
 }
 
@@ -31,45 +31,83 @@ func TextL(s *goquery.Selection) string {
 			noNl = n.NextSibling.Data
 		}
 
+		// Return bool if n.Data == "br" and then if it
+
+		// Because span will typically always have a lone child text node, we gotta explicitly handle it.
+		/* 		if n.Parent.Data == "span" {
+			builder.WriteString(n.Data)
+		} else */
 		if n.Type == html.TextNode && noNl != "br" {
 			builder.WriteString(n.Data + "\n")
 		} else if n.Type == html.TextNode {
 			builder.WriteString(n.Data)
+
 		}
-		if n.FirstChild != nil {
+
+		if n.FirstChild != nil { // go through all of n's children recursively.
 			for c := n.FirstChild; c != nil; c = c.NextSibling {
-				f(c)
+				f(c) // append string here based off bool value.
 			}
 		}
 	}
 	for _, n := range s.Nodes {
 		f(n)
 	}
-
 	return builder.String()
 }
 
+/* func TextLL(s *goquery.Selection) string {
+	var builder strings.Builder
+
+	// trim the front and end of the string.
+	var f func(*html.Node) bool
+	f = func(n *html.Node) bool {
+
+		// Return bool if n.Data == "br" and then if it
+		if n.Data == "br" {
+			return true
+		}
+		// Reason about this later (only an hour...)
+		// Is the issue w/ printint to do with using n within that for loop below?
+		if n.FirstChild != nil { // go through all of n's children recursively.
+			for c := n.FirstChild; c != nil; c = c.NextSibling {
+				brEncountered := f(c) // append string here based off bool value.
+
+				if n.Type == html.TextNode {
+					fmt.Println("I'm literally a textnode!")
+				}
+
+				if n.Type == html.TextNode && brEncountered == true {
+					builder.WriteString(n.Data)
+				} else if n.Type == html.TextNode && brEncountered == false {
+					builder.WriteString(n.Data + "\n")
+				}
+			}
+		}
+		return false
+	}
+	for _, n := range s.Nodes {
+		f(n)
+	}
+
+	return builder.String()
+} */
+
+// TODO: Make functions for each type of prayer -> Invitatory, office of readings etc...
+// TODO: Slap text etc.. into json then have a file w/ bubbletea.
+
+/* func invitatory(collect *)  */
+
 // c = new collector object. Manages network communicaiton and responsible for handling golang callbacks.
+
 func main() {
 	c := colly.NewCollector(
 		colly.AllowedDomains("divineoffice.org"),
 	)
 
-	//readings := make([]Reading, 0, 10)
-
 	cc := c.Clone()
 
-	/* 	c.OnHTML("div[class=sidebar-intro]", func(e *colly.HTMLElement) {
-		fmt.Println("In onHTML method.")
-
-		element := e.ChildText("h3")
-
-		if element == "" {
-			fmt.Println("Nothing found sorry!")
-		} else {
-			fmt.Println(element)
-		}
-	}) */
+	var body string
 
 	// common pattern -> have a default colly object go through websites and then have a clone of it scrape specific details.
 	// About today prayer and invatory and stuff has all the same class and -> has href which can be visited by c. detail scraper
@@ -89,50 +127,52 @@ func main() {
 
 		// selection object contains a slice of Node objects.
 		/* 		date := e.DOM.Find(".entry > p:nth-of-type(1)").Text() */
-		title := e.DOM.Find(".entry > h2").Text() // we can use goQuery to actually get stuff wow very nice.
+
+		/*		This thang not working blud...
+				title := e.DOM.Find("h1.intro-title")
+
+				if title.Length() == 0 {
+					fmt.Println("No elements found blud")
+					return
+				} */
 
 		selectionObj := e.DOM.Find(".entry > p") // This isn't ideal bcos we now don't have a seperation of paragraphs for prettier formatting.
-		body := TextL(selectionObj)
-
-		// placeholder variable until I set up the actual object!
-
-		/* 		pNodes := e.DOM.Find(".entry >p:nth-of-type(n+2)")
-		   		for _, v := range pNodes.Nodes {
-		   			if v.Type == html.TextNode {
-		   				fmt.Println(v.Data)
-		   			}
-		   		} */
-
-		// This is how .Text() is implemented make it append /n to EVERY element and use it instead of Text itself.
-
-		// this is the genral idea, but it's prolly not how to go about making a slice of paragraphs.
-		/* 		e.DOM.Find(".entry").Each(func(row int, s *goquery.Selection) {
-			paragraphs = append(paragraphs, s.Find("p").Text()) // hopefully dat works lol
-		}) */
+		body = TextL(selectionObj)
 
 		//fmt.Println(date)
-		fmt.Println(title)
 
 		fmt.Println(body)
 		// print out child elements.
 	})
 
-	/* 	c.OnHTML("a[href*=ord-]", func(e *colly.HTMLElement) {
+	/* 	c.OnHTML("a[href*=-or]", func(e *colly.HTMLElement) {
 	   		link := e.Attr("href")
 	   		fmt.Println("found link: ", link)
+
+	   		cc.Visit(link)
 	   	})
-	   	c.OnHTML("a[href*=-dp]", func(e *colly.HTMLElement) {
-	   		link := e.Attr("href")
-	   		fmt.Println("found link: ", link)
-	   	})
-	   	c.OnHTML("a[href*=-ep]", func(e *colly.HTMLElement) {
-	   		link := e.Attr("href")
-	   		fmt.Println("found link: ", link)
-	   	})
-	   	c.OnHTML("a[href*=-np-]", func(e *colly.HTMLElement) {
-	   		link := e.Attr("href")
-	   		fmt.Println("found link: ", link)
+
+	   	cc.OnHTML(".entry", func(e *colly.HTMLElement) {
+	   		selectionObj := e.DOM.Find(".entry > p") // This isn't ideal bcos we now don't have a seperation of paragraphs for prettier formatting.
+	   		body = TextL(selectionObj)
+
+	   		//fmt.Println(date)
+
+	   		fmt.Println(body)
+	   		// print out child elements.
 	   	}) */
+	/* 	   	c.OnHTML("a[href*=-dp]", func(e *colly.HTMLElement) {
+	   	   		link := e.Attr("href")
+	   	   		fmt.Println("found link: ", link)
+	   	   	})
+	   	   	c.OnHTML("a[href*=-ep]", func(e *colly.HTMLElement) {
+	   	   		link := e.Attr("href")
+	   	   		fmt.Println("found link: ", link)
+	   	   	})
+	   	   	c.OnHTML("a[href*=-np-]", func(e *colly.HTMLElement) {
+	   	   		link := e.Attr("href")
+	   	   		fmt.Println("found link: ", link)
+	   	   	}) */
 	/* 	c.OnHTML(".prayers-grid", func(e *colly.HTMLElement) {
 		fmt.Println("inside prayers-grid w")
 	}) */
